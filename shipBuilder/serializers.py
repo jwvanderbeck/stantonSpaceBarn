2b5c20e6dcab701ae2f4d6b7dd27e2300a19a9b6
John Vanderbeck <jwvanderbeck@gmail.com>
2013-11-30 08:38:40 -0800

    Reworking API for phase 2 and to go public

--- shipBuilder/serializers.py
+++ shipBuilder/serializers.py
@@ -8,27 +8,15 @@ class WithPkMixin(object):
 class ManufacturerSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Manufacturer
+		
+class VehicleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vehicle
+        
+class ItemPortSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ItemPort
 
-class HardpointSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Hardpoint
-
-class ItemSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Item
-
-class ItemCategorySerializer(serializers.ModelSerializer):
-	class Meta:
-		model = ItemCategory
-
-class ShipSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Ship
-
-class ImageSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Image
-
-class BuildSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Build
+class VehicleItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleItem
--- shipBuilder/views.py
+++ shipBuilder/views.py
@@ -14,84 +14,178 @@ class ManufacturerList(generics.ListAPIView):
 class ManufacturerDetail(generics.RetrieveAPIView):
 	queryset = Manufacturer.objects.all()
 	serializer_class = ManufacturerSerializer
+	
+# class VehicleList(generics.ListAPIView):
+#     queryset = Vehicle.objects.all()
+#     serializer_class = VehicleSerializer
+# 
+# class VehicleDetail(generics.RetrieveAPIView):
+#     queryset = Vehicle.objects.all()
+#     serializer_class = VehicleSerializer
 
+class VehicleItemList(generics.ListAPIView):
+    queryset = VehicleItem.objects.all()
+    serializer_class = VehicleItemSerializer
 
-class HardpointList(generics.ListAPIView):
-	queryset = Hardpoint.objects.all()
-	serializer_class = HardpointSerializer
+class VehicleItemDetail(generics.RetrieveAPIView):
+    queryset = VehicleItem.objects.all()
+    serializer_class = VehicleItemSerializer
 
-class HardpointDetail(generics.RetrieveAPIView):
-	queryset = Hardpoint.objects.all()
-	serializer_class = HardpointSerializer
-
-class ItemList(generics.ListAPIView):
-	queryset = Item.objects.all()
-	serializer_class = ItemSerializer
-
-class ItemDetail(generics.RetrieveAPIView):
-	queryset = Item.objects.all()
-	serializer_class = ItemSerializer
-
-class ItemCategoryList(generics.ListAPIView):
-	queryset = ItemCategory.objects.all()
-	serializer_class = ItemCategorySerializer
-
-class ItemCategoryDetail(generics.RetrieveAPIView):
-	queryset = ItemCategory.objects.all()
-	serializer_class = ItemCategorySerializer
-
-class ShipList(generics.ListAPIView):
-	queryset = Ship.objects.all()
-	serializer_class = ShipSerializer
-
-class ShipDetail(generics.RetrieveAPIView):
-	queryset = Ship.objects.all()
-	serializer_class = ShipSerializer
-
-
-class ImageList(generics.ListAPIView):
-	queryset = Image.objects.all()
-	serializer_class = ImageSerializer
-
-class ImageDetail(generics.RetrieveAPIView):
-	queryset = Image.objects.all()
-	serializer_class = ImageSerializer
-
-class BuildList(generics.ListAPIView):
-	queryset = Build.objects.all()
-	serializer_class = BuildSerializer
-
-class BuildDetail(generics.RetrieveAPIView):
-	queryset = Build.objects.all()
-	serializer_class = BuildSerializer
+# class ItemPortList(generics.ListAPIView):
+#     queryset = ItemPort.objects.all()
+#     serializer_class = ItemPortSerializer
+# 
+# class ItemPortDetail(generics.RetrieveAPIView):
+#     queryset = ItemPort.objects.all()
+#     serializer_class = ItemPortSerializer
 
 @api_view(('GET',))
 def api_root(request, format=None):
     return Response({
         'manufacturers': reverse('manufacturer-list', request=request, format=format),
-        'hardpoints': reverse('hardpoint-list', request=request, format=format),
-        'items': reverse('item-list', request=request, format=format),
-        'itemcategories': reverse('itemcategory-list', request=request, format=format),
-        'ships': reverse('ship-list', request=request, format=format),
-        'images': reverse('image-list', request=request, format=format),
-        'builds': reverse('build-list', request=request, format=format),
     })
 
+@api_view(('GET',))
+def vehicleList(request, format=None):
+    if request.method == 'GET':
+        vehicles = Vehicle.objects.all()
+        data = []
+        for vehicle in vehicles:
+            vehicleData = {
+                "id" : vehicle.id,
+                "vehicleClass" : vehicle.vehicleClass,
+                "name" : vehicle.name,
+                "displayName" : vehicle.displayName,
+                "upgradeSlots" : vehicle.upgradeSlots,
+                "maximumCrew" : vehicle.maximum_crew,
+                "emptyMass" : vehicle.empty_mass,
+                "length" : vehicle.length,
+                "width" : vehicle.width,
+                "height" : vehicle.height
+            }
+            if vehicle.category:
+                vehicleData['category'] = vehicle.category.name
+            if vehicle.manufacturer:
+                vehicleData['manufacturer'] = vehicle.manufacturer.name
+            
+            data.append(vehicleData)
+        return Response(data)
+@api_view(('GET',))
+def vehicleDetail(request, pk, format=None):
+    if request.method == 'GET':
+        vehicle = Vehicle.objects.get(pk=pk)
+        vehicleData = {
+            "id" : vehicle.id,
+            "vehicleClass" : vehicle.vehicleClass,
+            "name" : vehicle.name,
+            "displayName" : vehicle.displayName,
+            "upgradeSlots" : vehicle.upgradeSlots,
+            "maximumCrew" : vehicle.maximum_crew,
+            "emptyMass" : vehicle.empty_mass,
+            "length" : vehicle.length,
+            "width" : vehicle.width,
+            "height" : vehicle.height
+        }
+        if vehicle.category:
+            vehicleData['category'] = vehicle.category.name
+        if vehicle.manufacturer:
+            vehicleData['manufacturer'] = vehicle.manufacturer.name
 
-@api_view(['GET'])
-def hardpointListByClass(request, hardpoint_class, format=None):
-	if request.method == 'GET':
-		hardpoints = Hardpoint.objects.filter(hardpoint_class__exact=hardpoint_class)
-		serializer = HardpointSerializer(hardpoints, many=True)
-		return Response(serializer.data)
+        return Response(vehicleData)
 
-@api_view(['GET'])
-def itemsForHardpoint(request, hardpoint_id, format=None):
-	if request.method == 'GET':
-		hardpoint_class = Hardpoint.objects.get(pk=hardpoint_id).hardpoint_class
-		items = Item.objects.filter(hardpoint_class__gte=hardpoint_class)
-		serializer = ItemSerializer(items, many=True)
-		return Response(serializer.data)
+@api_view(('GET',))
+def itemPortList(request, format=None):
+    if request.method == 'GET':
+        itemPorts = ItemPort.objects.all()
+        data = []
+        for itemPort in itemPorts:
+            port = {}
+            port["id"] = itemPort.id
+            port["displayName"] = itemPort.displayName
+            port["name"] = itemPort.name
+            port["maxSize"] = itemPort.maxSize
+            port["minSize"] = itemPort.minSize
+            port["portClass"] = itemPort.portClass
+            port['supportedSubTypes'] = []
+            supported = []
+            # For each supported ItemType
+            for itemType in itemPort.supportedTypes.all():
+                # Look for supported ItemSubType with that parent, and add those found
+                supportedSubTypes = itemPort.supportedSubTypes.all().filter(parent__exact=itemType)
+                if len(supportedSubTypes) == 0:
+                    # if none found, add ALL itemSubType with that parent
+                    supportedSubTypes = VehicleItemSubType.objects.all().filter(parent__exact=itemType)
+                supported.extend(supportedSubTypes)
+            for subType in supported:
+                port['supportedSubTypes'].append(subType.subTypeName)
+            data.append(port)
+        return Response(data)
+@api_view(('GET',))
+def itemPortDetail(request, pk, format=None):
+    if request.method == 'GET':
+        itemPort = ItemPort.objects.get(pk=pk)
+        port = {}
+        port["id"] = itemPort.id
+        port["displayName"] = itemPort.displayName
+        port["name"] = itemPort.name
+        port["maxSize"] = itemPort.maxSize
+        port["minSize"] = itemPort.minSize
+        port["portClass"] = itemPort.portClass
+        port['supportedSubTypes'] = []
+        supported = []
+        # For each supported ItemType
+        for itemType in itemPort.supportedTypes.all():
+            # Look for supported ItemSubType with that parent, and add those found
+            supportedSubTypes = itemPort.supportedSubTypes.all().filter(parent__exact=itemType)
+            if len(supportedSubTypes) == 0:
+                # if none found, add ALL itemSubType with that parent
+                supportedSubTypes = VehicleItemSubType.objects.all().filter(parent__exact=itemType)
+            supported.extend(supportedSubTypes)
+        for subType in supported:
+            port['supportedSubTypes'].append(subType.subTypeName)
+        return Response(port)
+
+@api_view(('GET',))
+def itemPortListByVehicle(request, pk, format=None):
+    if request.method == 'GET':
+        itemPorts = ItemPort.objects.filter(parentVehicle__exact=pk)
+        data = []
+        for itemPort in itemPorts:
+            port = {}
+            port["id"] = itemPort.id
+            port["displayName"] = itemPort.displayName
+            port["name"] = itemPort.name
+            port["maxSize"] = itemPort.maxSize
+            port["minSize"] = itemPort.minSize
+            port["portClass"] = itemPort.portClass
+            port['supportedSubTypes'] = []
+            supported = []
+            # For each supported ItemType
+            for itemType in itemPort.supportedTypes.all():
+                # Look for supported ItemSubType with that parent, and add those found
+                supportedSubTypes = itemPort.supportedSubTypes.all().filter(parent__exact=itemType)
+                if len(supportedSubTypes) == 0:
+                    # if none found, add ALL itemSubType with that parent
+                    supportedSubTypes = VehicleItemSubType.objects.all().filter(parent__exact=itemType)
+                supported.extend(supportedSubTypes)
+            for subType in supported:
+                port['supportedSubTypes'].append(subType.subTypeName)
+            data.append(port)
+        return Response(data)        
+# @api_view(['GET'])
+# def hardpointListByClass(request, hardpoint_class, format=None):
+#   if request.method == 'GET':
+#       hardpoints = Hardpoint.objects.filter(hardpoint_class__exact=hardpoint_class)
+#       serializer = HardpointSerializer(hardpoints, many=True)
+#       return Response(serializer.data)
+# 
+# @api_view(['GET'])
+# def itemsForHardpoint(request, hardpoint_id, format=None):
+#   if request.method == 'GET':
+#       hardpoint_class = Hardpoint.objects.get(pk=hardpoint_id).hardpoint_class
+#       items = Item.objects.filter(hardpoint_class__gte=hardpoint_class)
+#       serializer = ItemSerializer(items, many=True)
+#       return Response(serializer.data)
 
 
 # @api_view(['GET'])
